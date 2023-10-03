31cb7e1db2cd42d1d5aebba975983a5aaae86a9a
David Inostroza <dinostrozars@gmail.com>
2023-10-03 19:09:42 -0300

    Update docs and endpoints

--- service.py
+++ service.py
@@ -13,23 +13,29 @@ from src import handlers, model
 app = Flask(__name__)
 
 
-@app.route('/hello/<name>', methods=['GET'])
-def say_hello(name):
-    """ Greeter Endpoint"""
-    resp = handlers.greeter(name)
-    return Response(resp, mimetype='plain/text')
+@app.route('/reservations', methods=['GET'])
+def reservations():
+    """ Get Reservations Endpoint"""
+    flight_id = request.args.get('flight_id')
+    resp = handlers.get_reservations(flight_id)
+    return jsonify(resp)
 
 # For more sophisticated forms in Flask, see:
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
-@app.route('/users', defaults={'user_id': ""}, methods=['POST'])
-@app.route('/users/<user_id>', methods=['POST'])
-def update_user(user_id):
-    """Endpoint that creates or saves user in Redis database"""
+@app.route('/reservations', methods=['PUT'])
+def reserve():
+    """Endpoint that reserves a seat for customer"""
     # Note 'force=True' ignores mime-type=app/json requirement default in Flask
-    user = request.get_json(force=True)
-
-    resp = handlers.save_user(user, user_id)
-    return jsonify(resp)
+    json_body = request.get_json(force=True)
+    resp = handlers.reserve(json_body)
+    if resp.get("status") == "success":
+        return jsonify(resp)
+    else:
+        return Response(
+            json.dumps(resp),
+            status=403,
+            mimetype='application/json'
+        )
 
 
 def init():
--- src/handlers.py
+++ src/handlers.py
@@ -10,12 +10,10 @@ def greeter(name):
     """Greeter route callback"""
     return f"Hello {name}!"
 
-def save_user(user, user_id=""):
-    """Save User route callback"""
-    if user_id == "":
-        user_id = str(uuid.uuid4())
-    log.info("Saving USER_ID: %s", user_id)
+def reserve(json_body):
+    """Save reservation callback"""
+    return model.save_reservation(json_body)
 
-    user = json.dumps(user)
-
-    return model.save_user(user, user_id)
+def get_reservations(flight_id):
+    """"Get reservations callback"""
+    return model.get_reservations(flight_id)
